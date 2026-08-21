73808049f4ff5f982e70daa67f3e23aff5710078
khan789skq-sketch <khan789skq@gmail.com>
2026-08-21 23:19:56 +0400

    Initialize Firebase and set up authentication

--- EIFM-WCC-PPM/firebase.js
+++ EIFM-WCC-PPM/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCgbcK1uunx4FPtmS3uf3i_MyvmaDoDufg",
   authDomain: "eifm-wcc-ppm.firebaseapp.com",
@@ -6,3 +9,9 @@ const firebaseConfig = {
   messagingSenderId: "641028866264",
   appId: "1:641028866264:web:36ef50f9e3e231b09b17ad"
 };
+
+export const firebaseConfigured = !firebaseConfig.apiKey.startsWith("PASTE_");
+
+export const auth = firebaseConfigured
+  ? getAuth(initializeApp(firebaseConfig))
+  : null;
