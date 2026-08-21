f3752702074cef27f4e90e4cf13574569a0844be
khan789skq-sketch <khan789skq@gmail.com>
2026-08-21 21:48:08 +0400

    Update Firebase configuration with real credentials

--- EIFM-WCC-PPM/firebase.js
+++ EIFM-WCC-PPM/firebase.js
@@ -1,9 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-const firebaseConfig={
-  apiKey:"PASTE_API_KEY",authDomain:"PASTE_PROJECT.firebaseapp.com",
-  projectId:"PASTE_PROJECT_ID",storageBucket:"PASTE_PROJECT.firebasestorage.app",
-  messagingSenderId:"PASTE_SENDER_ID",appId:"PASTE_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyCgbcK1uunx4FPtmS3uf3i_MyvmaDoDufg",
+  authDomain: "eifm-wcc-ppm.firebaseapp.com",
+  projectId: "eifm-wcc-ppm",
+  storageBucket: "eifm-wcc-ppm.firebasestorage.app",
+  messagingSenderId: "641028866264",
+  appId: "1:641028866264:web:36ef50f9e3e231b09b17ad"
 };
-export const firebaseConfigured=!firebaseConfig.apiKey.startsWith("PASTE_");
-export const auth=firebaseConfigured?getAuth(initializeApp(firebaseConfig)):null;
